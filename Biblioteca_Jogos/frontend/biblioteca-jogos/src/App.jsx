042110445a4c36dde74a389f19a490f51d6aa063
F0RT-DEV <douglasf935255@gmail.com>
2025-02-19 06:19:59 -0300

    Subindo o esqueleto do projeto

---
+++ Biblioteca_Jogos/frontend/biblioteca-jogos/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import AddGame from "./pages/AddGame";
+import AddGenre from "./pages/AddGenre";
+import CadastrarDev from "./pages/CadastrarDev";
+import MostrarDev from "./pages/MostrarDev";
+
+
+export default function App() {
+  return (
+    <div>
+      <nav className="nav-bar">
+        <Link to="/">Home</Link>
+        <Link to="/add-game">Adicionar Jogo</Link>
+        <Link to="/add-genre">Adicionar Gênero</Link>
+        <Link to="/add-cadastrar-dev">Cadastrar Desenvolvedor</Link>
+        <Link to="/mostrar-dev">Mostrar Desenvolvedor</Link>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastrar-dev" element={<CadastrarDev />} />
+        <Route path="/mostrar-dev" element={<MostrarDev />} />
+        <Route path="/add-game" element={<AddGame />} />
+        <Route path="/add-genre" element={<AddGenre />} />
+      </Routes>
+    </div>
+  );
+}
